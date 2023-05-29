a813cc374042fe18908fa35aee09997d7479e0c3
sm222 <soleil.maraska@gmail.com>
2023-05-29 11:26:37 -0400

    chnage realloc

--- lib/lib_ft/ft_realloc.c
+++ lib/lib_ft/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: anboisve <anboisve@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 13:46:45 by anboisve          #+#    #+#             */
-/*   Updated: 2023/05/26 10:31:50 by anboisve         ###   ########.fr       */
+/*   Updated: 2023/05/29 11:14:31 by anboisve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 /*
 realloc give you the size of the memory you want on the same poiter
 */
-void	*ft_realloc(void *old, size_t count, size_t size, size_t new_size)
+void	*ft_realloc(void *old, size_t count, size_t len, size_t new_len)
 {
 	void	*new;
 
 	if (!old)
 		return (NULL);
-	new = ft_calloc(count, new_size);
+	new = ft_calloc(count, new_len);
 	if (!new)
 		return (NULL);
-	if (size > new_size)
-		ft_memcpy(new, old, count * new_size);
+	if (len <= new_len)
+		ft_memcpy(new, old, count * len);
 	else
-		ft_memcpy(new, old, count * size);
+		ft_memcpy(new, old, count * new_len);
 	ft_free(old);
 	return (new);
 }
