2d27ba32f735df7b7ad19624b7b0bc38648e40fc
sm222 <soleil.maraska@gmail.com>
2023-07-23 17:59:36 -0400

    working on export

--- built_in/export/export.c
+++ built_in/export/export.c
@@ -1,19 +1,55 @@
 
 #include "export.h"
 
-void	print_env(char **en, int re_out)
+int	print_env(char **en, int re_out)
 {
 	size_t	i;
 
 	i = 0;
 	while (en && en[i])
 		ft_printf(re_out, "%odeclare -x %s\n", NULL ,en[i++]);
+	return (EXIT_SUCCESS);
 }
+
+short	look_for_dup(char **env, char *name)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (env && env[i])
+	{
+		len = 0;
+		while (env[i][len] && env[i][len] != '=')
+			len++;
+		if (ft_strncmp(env[i], name, len + 1) == 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+short	export_new(char **en)
+{
+	
+}
+
 
 int	ft_export(char **av, int re_in, int re_out, char **en)
 {
+	size_t	i;
+
+	i = 1;
 	(void)re_in;
+	if (!en)
+		en = ft_return_ptr(NULL, ENV_C);
+	if (!en)
+		return (2);
 	if (ft_strlen_double(av) == 1)
-		print_env(en, re_out);
+		return (print_env(en, re_out));
+	while (av[i])
+	{
+
+	}
 	return (EXIT_SUCCESS);
 }
