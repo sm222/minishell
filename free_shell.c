f99e0ba0c8363f7311fb4f2a97938eb237ca1922
sm222 <soleil.maraska@gmail.com>
2023-09-26 11:40:58 -0400

    working on ft

---
+++ free_shell.c
@@ -0,0 +1,14 @@
+#include "include/minishell.h"
+
+short static	free_data_shell(t_mshell *data)
+{
+	data->s
+}
+
+short	clean_shell(void(*f) (void *), void *data)
+{
+	if (!f)
+		return (BAD_ARGS);
+	f(data);
+
+}
