b1da80a4ba44c05a5ed4925afe98e6d402d9b02c
Brian Rheaume-Michaud <brheaume@c2r4p8.42quebec.com>
2023-10-05 17:43:29 -0400

    fixed empty file with redirections & rearranged header file

--- parsing/parsing.h
+++ parsing/parsing.h
@@ -16,10 +16,9 @@
 # define CORRECT 1
 # define INVALID -1 
 # define INCORRECT 0
-# define CHAR_LIMIT 32 // space is the last char not included
 # define FIRST_INDEX 0
 # define PASSED_QUOTES 29
-# define IGNORE_QUOTES 92 //30
+# define IGNORE_QUOTES 30
 # define PASSED_THROUGH 31
 # define SINGLE_REDIRECT 115 // ascii value of 's'
 # define DOUBLE_REDIRECT 100 // ascii value of 'd'
@@ -52,37 +51,43 @@ typedef struct s_rdct
 } t_rdct;
 
 int		ft_verify(char *src);
-void	ft_purge(t_loc *list);
+t_loc	*ft_validate_cmds(t_loc *list);
+
 t_loc	*ft_parsing(char *src);
+void	ft_add_loc(t_loc **list, char **cmd, t_token *tokens);
+
 int		ft_has_pipe(char *src);
-void	ft_quote_op(char *cmd);
-int		ft_arraylen(char **src);
-int		ft_has_quotes(char *src);
-int		ft_quote_error(char *src);
 int		ft_invalid_pipe(char *cmd);
-int		ft_has_redirect(char *src);
+void	ft_pipe_op(char *cmd, t_loc **list);
+
+void	ft_quote_op(char *cmd);
+int		ft_has_quotes(char *src);
+t_idx	ft_quotes_delimitation(char *src);
+int		ft_is_not_in_quotes(char *src, int i);
+
+int		ft_arraylen(char **src);
 void	*ft_clear_array(char **src);
-int		ft_redirect_index(char *src);
+char	**ft_arrayjoin(char **src, char *s);
+
+void	ft_purge(t_loc *list);
 void	ft_pass_through(char **decon);
 int		ft_at_index(char *src, char c);
-t_loc	*ft_validate_cmds(t_loc *list);
-int		ft_redirect_rev_index(char *src);
-t_idx	ft_quotes_delimitation(char *src);
 int		ft_at_rev_index(char *src, char c);
-char	*ft_get_file(char *cmd, char mode);
-void	ft_pipe_op(char *cmd, t_loc **list);
-char	**ft_arrayjoin(char **src, char *s);
-int		ft_is_not_in_quotes(char *src, int i);
-int		ft_check_here_doc(char *src, t_rdct *fd);
-int		ft_redirect_op(char *cmd, t_token *tokens);
 char	*ft_strslice(char *src, int start, int end);
-char	*ft_file_extract(char *src, int start_index);
-int		ft_here_doc(char *src, int start, t_rdct *fd);
-int		ft_check_ignore(char *src, int start, int end);
-char	**ft_cmd_deconstruct(char *cmd, t_token *tokens);
-void	ft_add_loc(t_loc **list, char **cmd, t_token *tokens);
+
+int		ft_has_redirect(char *src);
+int		ft_redirect_op(char *cmd, t_token *tokens);
 int		ft_input(char *src, t_token *tokens, int start_index);
 int		ft_output(char *src, t_token *tokens, int start_index);
+
+char	*ft_get_file(char *cmd, char mode);
+char	*ft_file_extract(char *src, int start_index);
 int		ft_file_op(char *path, t_token *tokens, char redirect, char duplicity);
+
+int		ft_here_doc(char *src, t_rdct *fd);
+int		ft_check_here_doc(char *src, t_idx *i_doc);
+
+void	ft_word_delimiter(char *cmd, t_idx *index);
+char	**ft_cmd_deconstruct(char *cmd, t_token *tokens);
 
 #endif
