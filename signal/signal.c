839809d7e4a02e5ccad9045a15bea4df8f9155ae
Brian Rheaume-Michaud <brheaume@c1r8p9.42quebec.com>
2023-10-09 13:34:35 -0400

    swapped signal file into signal dir

---
+++ signal/signal.c
@@ -0,0 +1,52 @@
+#include "parsing.h"
+
+void	ft_sig_handle(int signal)
+{
+	(void)signal;
+	ft_putendl_fd("\0", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void	ft_child_sig_handle(int signal)
+{
+	if (signal == SIGQUIT)
+	{
+		ft_putendl_fd("\0", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+	else if (signal == SIGINT)
+	{
+		ft_putendl_fd("\0", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+}
+
+void	ft_doc_sig_handle(int signal)
+{
+	(void)signal;
+	ft_putendl_fd("\0", 1);
+	// free all cmds
+}
+
+void	ft_signal_handler(int mode)
+{
+	if (mode == CMD)
+	{
+		signal(SIGINT, &ft_sig_handle);
+		signal(SIGQUIT, SIG_IGN)
+	}
+	else if (mode == CHILD)
+	{
+		signal(SIGINT, &ft_child_sig_handle);
+		signal(SIGQUIT, &ft_child_sig_handle)
+	}
+	else if (mode == HERE_DOC)
+	{
+		signal(SIGINT, &ft_doc_sig_handle);
+		signal(SIGQUIT, SIG_IGN)
+	}
+}
