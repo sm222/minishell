10b5d0d4f9debbb9721ba31ac0e5f47115b8afe5
Brian Rheaume-Michaud <brheaume@c2r4p8.42quebec.com>
2023-10-12 12:59:02 -0400

    fixed pipe

--- parsing/pipe.c
+++ parsing/pipe.c
@@ -15,24 +15,26 @@ int	ft_has_pipe(char *src)
 	return (INCORRECT);
 }
 
-void	ft_init_start_cmd(char *cmd, t_loc **list)
+void	ft_init_start_cmd(char *cmd, t_idx *i, t_loc **list)
 {
-	t_idx	index;
 	t_token	*tokens;
 	char	**decon;
 
 	ft_add_node(list);
-	index.start_index = FIRST_INDEX;
+	if (i->end_index > 0)
+		i->start_index = i->end_index;
+	else
+		i->start_index = FIRST_INDEX;
 	tokens = ft_calloc(NODE, sizeof(t_token));
 	ft_set_tokens(list, tokens);
 	if (ft_has_pipe(cmd))
 	{
-		index.end_index = ft_at_index(cmd, '|');
-		cmd[index.end_index] = PASSED_THROUGH;
+		i->end_index = ft_at_index(cmd, '|');
+		cmd[i->end_index] = PASSED_THROUGH;
 	}
 	else
-		index.end_index = (int)ft_strlen(cmd);
-	(*list)->slice = ft_strslice(cmd, index.start_index, index.end_index);
+		i->end_index = (int)ft_strlen(cmd);
+	(*list)->slice = ft_strslice(cmd, i->start_index, i->end_index);
 	decon = ft_cmd_deconstruct((*list)->slice, (*list)->tokens);
 	ft_set_decon(list, decon);
 	ft_free((*list)->slice);
@@ -40,7 +42,10 @@ void	ft_init_start_cmd(char *cmd, t_loc **list)
 
 void	ft_pipe_op(char *cmd, t_loc **list)
 {
+	t_idx	index;
+
+	ft_bzero(&index, sizeof(t_idx));
 	while (ft_has_pipe(cmd))
-		ft_init_start_cmd(cmd, list);
-	ft_init_start_cmd(cmd, list);
+		ft_init_start_cmd(cmd, &index, list);
+	ft_init_start_cmd(cmd, &index, list);
 }
