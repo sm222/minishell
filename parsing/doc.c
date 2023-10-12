acbc3dec9e73d1d640e7835e2448d5f6c992916b
Brian Rheaume-Michaud <brheaume@c2r4p8.42quebec.com>
2023-10-12 15:12:38 -0400

    freed & set to NULL here_doc file

--- parsing/doc.c
+++ parsing/doc.c
@@ -79,7 +79,7 @@ static int	ft_run_here_doc(char *src, t_idx *limit, t_rdct *fd)
 	ft_return_ptr(file, DOC_FILE);
 	printf("doc file: %s\n", file);
 	fd_doc = make_here_doc(no_quotes, file);
-	free(file);
+	file = ft_free(file);
 	return (fd_doc);
 }
 
